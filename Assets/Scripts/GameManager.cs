e005042c5ed5b79a2a89a33c727d38d0718bdb22
Ceci Tran <ctdog94@yahoo.com>
2015-03-05 19:39:33 -0800

    GameManagerScript
    Hi

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,11 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
-	private GameObject Player;
-	private float initialPlayerYPos;
-	public List<GameObject> events = new List<GameObject>();
-	private float Score;
-	private bool courRunning = false;
+	private GameObject Player; //the player
+	private float initialPlayerYPos; //the initial player position
+	public List<GameObject> events = new List<GameObject>(); //event array
+	private float Score; // score count
+	private bool courRunning = false; //to check to see if the courantine method is running
+	// so that it doesn't create a extra amount of objects (any logs, clowns, etc.)
 
 	void Start() {
 		Player = GameObject.FindGameObjectWithTag ("Player");
@@ -30,16 +31,18 @@ public class GameManager : MonoBehaviour {
 		}
 	}
 
+	//Random generator 
 	IEnumerator generateEvent() {
-		yield return new WaitForSeconds (2f);
+		yield return new WaitForSeconds (2f); //executes after every two seconds
 		if (Player != null) {
 			Instantiate(events[Random.Range(0, events.Count)], new Vector2(Player.transform.position.x + 100f, initialPlayerYPos), Quaternion.identity);
 		}
-		courRunning = false;
+		courRunning = false; //set to false to not invoke this method each frames
 	}
 
+	//text for the score
 	void OnGUI() {
-		string poop = Score.ToString ("#");
+		string poop = Score.ToString ("#"); //#.0 makes the decimal
 		GUI.Label (new Rect (50, 75, 100, 100), "Score: " + poop);
 	}
 }
